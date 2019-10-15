0e4057a36993566b0085a7b264882e1938a518d0
Leifhaa <Leifhaa@hotmail.com>
2019-10-15 17:53:53 +0200

    Inplemented postgres db

--- src/main/java/no/kristiania/jdbc/ProductDao.java
+++ src/main/java/no/kristiania/jdbc/ProductDao.java
@@ -1,36 +1,68 @@
 package no.kristiania.jdbc;
 
+import org.postgresql.ds.PGSimpleDataSource;
+
 import javax.sql.DataSource;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.Scanner;
 
 public class ProductDao {
-    private List<String> products = new ArrayList<>();
     private DataSource dataSource;
 
     public ProductDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    public void insertProduct(String productName) {
-        products.add(productName);
-
-        try (Connection conn = dataSource.getConnection();){
-            PreparedStatement statement = conn.prepareStatement("insert into products(name) value(?)");
-            statement.setString(1,productName);
+    public void insertProduct(String productName) throws SQLException {
+        try (Connection conn = dataSource.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(
+                    "insert into products(name) values(?)");
+            statement.setString(1, productName);
             statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
-
     }
 
+    public List<String> listAll() throws SQLException {
+        try (Connection connection = dataSource.getConnection()){
+            try (PreparedStatement statement = connection.prepareStatement (
+                    "select * from products"))
+            {
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    List<String> result = new ArrayList<>();
+                    while (resultSet.next()){
+                        result.add(resultSet.getString("name"));
+                    }
+
+                    return result;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws SQLException, IOException {
+        System.out.println("Enter a product name:");
+        String productName = new Scanner(System.in).nextLine();
+
+        Properties properties = new Properties();
+        properties.load(new FileReader("webshop.properties"));
 
 
-    public List<String> listAll() {
-       return products;
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/webshop");
+        dataSource.setUser("webshop");
+        dataSource.setPassword("NWS9Sta;U]9;4r");
+        ProductDao productDao = new ProductDao(dataSource);
+        productDao.insertProduct(productName);
+
+        System.out.println(productDao.listAll());
     }
 }
--- src/test/java/WebShopTest.java
+++ src/test/java/WebShopTest.java
@@ -2,14 +2,21 @@ import no.kristiania.jdbc.ProductDao;
 import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WebShopTest {
     @Test
-    void shouldRetrieveStoredProduct(){
-        ProductDao dao = new ProductDao(new JdbcDataSource());
+    void shouldRetrieveStoredProduct() throws SQLException {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:test");
+        dataSource.getConnection().createStatement().executeUpdate(
+                "create table products (name varchar(100))"
+        );
+
+        ProductDao dao = new ProductDao(dataSource);
         String productName = pickOne(new String[]{"Apples","Bananas", "Pelee"});
         dao.insertProduct(productName);
         assertThat(dao.listAll()).contains(productName);
